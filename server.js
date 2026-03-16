a14a973a36818a8e7ff9ce5e8c850dc4acce5417
Gustavo Cañete <gustavo@brokenrubik.co>
2026-03-15 23:44:49 -0300

    update allow origin and frame ancestors values

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.use(express.static('build'));
 
 // En src/server.ts, actualizar CORS:
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.STOREURL || '*');
+  res.header('Access-Control-Allow-Origin', process.env.STOREBASEURL || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   // Permitir iframe
   res.header('X-Frame-Options', 'ALLOWALL');
-  res.header('Content-Security-Policy', `frame-ancestors 'self' ${process.env.STOREURL}`);
+  res.header('Content-Security-Policy', `frame-ancestors 'self' ${process.env.STOREBASEURL}`);
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
